30edfa7bcfa8a46592c76c2409a0bd6e7c4deaee
dispherical <62346025+dispherical@users.noreply.github.com>
2025-10-15 22:27:59 -0400

    add generator, icons, now playing, animations, map background

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio');
+const icons = require("./icons")
+module.exports = async function (eleventyConfig) {
+  const { RenderPlugin } = await import("@11ty/eleventy");
+  eleventyConfig.addPlugin(RenderPlugin);
+
+  eleventyConfig.addTransform("externalFavicon", async function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      const $ = cheerio.load(content);
+      const anchors = $("a[href^='http']").toArray()
+      await Promise.all(anchors.map(async (el) => {
+        const $a = $(el);
+        if ($a.find("img").length > 0) return;
+        const href = $a.attr("href");
+        if (href && !href.includes("https://unsplash.com") && !href.includes("https://cdn.dispherical.com") && !href.includes("https://blog.dispherical.com")) {
+          const faviconUrl = await icons(href)
+          $a.prepend(`<img src="${faviconUrl}" alt="favicon" style="width:1em;height:1em;vertical-align:middle;margin-right:0.25em;">`);
+
+        }
+
+      }))
+
+      return $.html();
+    }
+    return content;
+  });
+
+        eleventyConfig.addPassthroughCopy("icons");
+
+}
---
+++ icons.js
@@ -0,0 +1,64 @@
+// From https://github.com/simple-icons/simple-icons/blob/develop/sdk.mjs
+const TITLE_TO_SLUG_REPLACEMENTS = {
+  '+': 'plus',
+  '.': 'dot',
+  '&': 'and',
+  đ: 'd',
+  ħ: 'h',
+  ı: 'i',
+  ĸ: 'k',
+  ŀ: 'l',
+  ł: 'l',
+  ß: 'ss',
+  ŧ: 't',
+  ø: 'o',
+};
+
+const TITLE_TO_SLUG_CHARS_REGEX = new RegExp(
+  `[${Object.keys(TITLE_TO_SLUG_REPLACEMENTS).join('')}]`,
+  'g',
+);
+
+const TITLE_TO_SLUG_RANGE_REGEX = /[^a-z\d]/g;
+
+const titleToSlug = (title) =>
+  title
+    ?.toLowerCase()
+    ?.replaceAll(
+      TITLE_TO_SLUG_CHARS_REGEX,
+      (char) => TITLE_TO_SLUG_REPLACEMENTS[char],
+    )
+    ?.normalize('NFD')
+    ?.replaceAll(TITLE_TO_SLUG_RANGE_REGEX, '');
+
+const fs = require('fs').promises;
+const path = require('path');
+module.exports = async function (url) {
+  var originDomain = new URL(url).hostname.split('.').slice(-2).join('.')
+  const icons = await (await fetch("https://rawcdn.githack.com/simple-icons/simple-icons/refs/heads/develop/data/simple-icons.json")).json()
+  var foundIcon = icons.find(icon => {
+    return new URL(icon.source).hostname.split('.').slice(-2).join('.') == originDomain
+  })
+  if (originDomain == "github.com") foundIcon = icons.find(icon => icon.title == "GitHub")
+  if (originDomain == "bsky.app") foundIcon = icons.find(icon => icon.title == "Bluesky")
+  if (originDomain == "dino.icu") foundIcon = icons.find(icon => icon.title == "Mastodon")
+  if (originDomain == "keyoxide.org") return "https://design.keyoxide.org/keyoxide/logos/keyoxide.rounded.2x.png"
+  if (foundIcon) {
+    const slug = titleToSlug(foundIcon?.title || "");
+    const svgUrl = `https://cdn.jsdelivr.net/npm/simple-icons@v15/icons/${slug}.svg`;
+    const res = await fetch(svgUrl);
+    let svg = await res.text();
+    svg = svg.replace(
+      /<svg([^>]+)>/,
+      `<svg$1 fill="#${foundIcon.hex}">`
+    );
+
+    const iconsDir = path.join(__dirname, 'icons');
+    await fs.mkdir(iconsDir, { recursive: true });
+    const filePath = path.join(iconsDir, `${slug}.svg`);
+    await fs.writeFile(filePath, svg, 'utf8');
+    return `/icons/${slug}.svg`
+   
+  }
+  else return `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`
+}
